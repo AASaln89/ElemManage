600e2ade072408c9cd7332219fc58196b6110ec8
Andrey Salnikov <kvanta13@yandex.ru>
2022-01-18 01:07:39 +0300

    Добавьте файлы проекта.

---
+++ ElemManage/Commands_app.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ElemManage
+{
+    class Commands_app
+    {
+        public static RoutedUICommand ExitApp { get; set; }
+        static Commands_app ()
+        {
+            InputGestureCollection input1 = new InputGestureCollection();
+            input1.Add(new KeyGesture(Key.P, ModifierKeys.Control, "Ctrl+P"));
+            ExitApp = new RoutedUICommand("Выход","ExitApp", typeof(Commands_app),input1);
+        }
+    }
+}
